a79e9d0d49eeb3b7d9e9f8e100958a4c6767b3dd
Sanja <apofig@gmail.com>
2014-07-19 08:11:17 +0300

    Удалил странички

--- jbehave-sample/src/test/resources/amazon.homepage_files/iframeproxy-9_data/amzn.js
+++
@@ -1,6 +0,0 @@
-document.write('');
-
-window.showAdSlug=true;
-window.aanResponse={adId:'227810883',creativeId:'37968347'};
-
-document.write('<body>\n\n<script language=\'javascript\'>\nvar slot = \'right-2\';\nvar aanParams = {\'right-2\':\"site=amazon.us;pt=Gateway;slot=right-2;ef=0\"};\n<\/script>');document.write('\n\n<script src=\"http://g-ecx.images-amazon.com/images/G/01/pda/pda.js\"><\/script>');document.write('\n\n</body>\n</html></div>\n</div>');
